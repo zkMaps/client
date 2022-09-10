db22dadf0609e9ca794ab9d785946e73c6caa8bf
Franco Petra <franco.petraz@gmail.com>
2022-09-10 20:29:08 -0300

    📦 NEW: Private contract

--- packages/react-app/src/constants/areas.js
+++ packages/react-app/src/constants/areas.js
@@ -29,4 +29,32 @@ export default [
     precision: 5,
     protocol: "groth16",
   },
+  {
+    id: 2,
+    description: "RayTracing6Private - Argentina - Public Polygon",
+    wasmFile:
+      "https://ipfs.io/ipfs/QmeLREkAuCTLMkyc37CqPF44SpL5PStDQeERazCAinjFBQ/RayTracing6Private_js/RayTracing6Private.wasm",
+    zkeyFile: "https://ipfs.io/ipfs/QmeLREkAuCTLMkyc37CqPF44SpL5PStDQeERazCAinjFBQ/RayTracing6Private_0001.zkey",
+    publicConstraint: ["1"],
+    geoJson: {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [-66.31347656250001, -31.72816714602391],
+            [-65.08300781250001, -28.88315960932351],
+            [-55.10742187500001, -35.49645605658411],
+            [-61.65527343750001, -37.68382032669381],
+            [-63.45703125000001, -35.06597313798411],
+            [-66.13769531250001, -35.10193405724601],
+          ],
+        ],
+      },
+    },
+    contract: contracts["0xDEc4bD77b257E346F2fa8E52C4B91482C9081e6b"], // Mumbai | 6 Vertex | Public | Emits
+    precision: 5,
+    protocol: "groth16",
+  },
 ];
