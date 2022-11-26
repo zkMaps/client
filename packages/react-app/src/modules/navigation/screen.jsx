4ae5cc15bc22c0e482c527f7fa8fa33216659090
Franco Petra <franco.petraz@gmail.com>
2022-11-26 13:55:54 +0000

    👌 IMPROVE: cleanup

--- packages/react-app/src/modules/navigation/screen.jsx
+++ packages/react-app/src/modules/navigation/screen.jsx
@@ -3,7 +3,6 @@ import React from "react";
 // Components
 import NavBar from "../../components/navBar";
 import Home from "../home";
-// import Transfers from "../transfers";
 import Profile from "../profile";
 import Subgraph from "../../views/Subgraph";
 
@@ -12,9 +11,6 @@ const GeneralNavigation = () => {
     <div style={{ height: "100vh" }}>
       <NavBar />
       <Home path="/" />
-      {/* <Markers path="markers"/> */}
-      {/* <Transfers path="transfers" /> */}
-      {/* <Profile path="profile" /> */}
       {/* TODO: make available only when testing */}
       <Subgraph path="subgraph" />
     </div>
